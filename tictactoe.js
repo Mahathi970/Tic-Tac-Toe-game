9bd7ceb12408cae434d9f99a75dc0a95517ba357
Mahathi970 <69047688+Mahathi970@users.noreply.github.com>
2021-08-18 11:13:46 +0530

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -32,27 +32,4 @@ for(var i = 0;i<squares.length;i++){
     squares[i].addEventListener("click",change);
 }   
 
-
-
-    // Checking if Player X won or not and after
-    // that disabled all the other fields
-    if ((b1 == 'x' || b1 == 'X') && (b2 == 'x' ||
-        b2 == 'X') && (b3 == 'x' || b3 == 'X')) {
-        document.getElementById('print')
-            .innerHTML = "Player X won";
-        window.alert('Player X won');
-    }
-    else if ((b1 == 'x' || b1 == 'X') && (b4 == 'x' ||
-        b4 == 'X') && (b7 == 'x' || b7 == 'X')) {
-        document.getElementById('print')
-            .innerHTML = "Player X won";
-        window.alert('Player X won');
-    }
-    else if ((b7 == 'x' || b7 == 'X') && (b8 == 'x' ||
-        b8 == 'X') && (b9 == 'x' || b9 == 'X')) {
-        document.getElementById('print')
-            .innerHTML = "Player X won";
-      
-        window.alert('Player X won');
-    }
     
